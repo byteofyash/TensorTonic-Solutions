ef1a46f51e60ae6702d31c169cfa9f23c236f72a
Yash Ranjan <yashranjan700@gmail.com>
2026-07-06 13:19:41 +0530

    Solve pandas-groupby-basics

---
+++ study-plans/pandas-basics/pandas-groupby-basics/pandas-groupby-basics.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def groupby_basics(data, group_col, value_col):
+    """
+    Returns: dict with 'sum', 'mean', 'count' (each a dict)
+    """
+    df = pd.DataFrame(data)
+    sum = df.groupby(group_col)[value_col].sum().to_dict()
+    mean = df.groupby(group_col)[value_col].mean().to_dict()
+    count = df.groupby(group_col)[value_col].count().to_dict()
+
+
+
+    return{
+        "sum" : sum,
+        "mean" : mean, 
+        "count" : count
+    }
+    
