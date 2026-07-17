0b57af455d75f63d4c89a317efe7fcdf4d6f9c1b
Yash Ranjan <yashranjan700@gmail.com>
2026-07-17 23:31:56 +0530

    Solve sql-having-clause

---
+++ study-plans/sql-basics/sql-having-clause/sql-having-clause.sql
@@ -0,0 +1,14 @@
+-- Write your SQL query here
+select
+ customer,
+COUNT(product) as total_orders, 
+SUM(amount) as total_spent
+    
+from orders
+group by customer
+having total_orders >=2
+
+
+order by
+total_spent
+desc;
